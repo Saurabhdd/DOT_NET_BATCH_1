475bce661ab336a1d98ee6e6b75446dc695deae7
Saurabhdd <saurabh.deshpande1992@gmail.com>
2024-09-03 20:52:39 +0530

    Basic product laptop

---
+++ BasicProduct/Laptop.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace LaptopProgram
+{
+    internal class Laptop
+    {
+        private string cpuType;
+        private int ram;
+        private int secondaryStorage;
+        private int gpuMemory;
+        private string displayType;
+        private string brand;
+        private const string modelName;
+
+
+
+        public bool InstallOS(){
+
+        }
+
+        public void UpdateROM(){
+
+        }
+
+        public void IncreaseRAM(){
+
+        }
+
+        public void On(){
+
+        }
+
+        public void Off(){
+
+        }
+
+        private void CheckPrimaryForWindows(){
+
+        }
+
+        private void OpenA20Gate(){
+
+        }
+
+        private bool DownloadROMFromCorrectSource(){
+
+        }
+
+        private void TweakBatteryBetweenAC_DC(){
+
+        }
+    }
+}
---
+++ BasicProduct/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LaptopProgram;
+
+namespace MainProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop();
+        }
+    }
+}
