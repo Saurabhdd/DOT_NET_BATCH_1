b46e303e2799f1df5cf15277011b2b3601bc8859
TejasMore99 <105068844+TejasMore99@users.noreply.github.com>
2024-09-17 15:47:11 +0530

    Product with inheritance and interface implementaion.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AllinOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CTable table1 = new CTable("Dining Table", "Furniture", 299.99, "Wood", 4);
+            CTable table2 = new CTable("Coffee Table", "Furniture", 99.99, "Glass", 3);
+            CTable table3 = new CTable("Study Table", "Furniture", 150.00, "Metal", 4);
+
+            pageDisplay<CTable> tableDisplay = new pageDisplay<CTable>();
+
+            tableDisplay.AddProduct(table1);
+            tableDisplay.AddProduct(table2);
+            tableDisplay.AddProduct(table3);
+
+            Console.WriteLine("All Products:");
+            tableDisplay.DisplayProducts();
+
+            Console.WriteLine("Filtered Products (Price > 100):");
+            tableDisplay.DisplayFilteredProducts(table => table.Price > 100);
+
+            Console.WriteLine("Filtered Products (Material is Wood):");
+            tableDisplay.DisplayFilteredProducts(table => table.Material == "Wood");
+        }
+    }
+}
